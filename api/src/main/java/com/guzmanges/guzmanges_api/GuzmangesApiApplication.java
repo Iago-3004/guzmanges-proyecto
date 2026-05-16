537d7ad3b03375703add07722570e9ca77a0a95e
Iago-3004 <iagoguzman06@gmail.com>
2026-05-16 21:10:17 +0200

    Inicialización de API + APP Flutter

---
+++ api/src/main/java/com/guzmanges/guzmanges_api/GuzmangesApiApplication.java
@@ -0,0 +1,13 @@
+package com.guzmanges.guzmanges_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuzmangesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuzmangesApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/guzmanges/guzmanges_api/GuzmangesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guzmanges.guzmanges_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuzmangesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
